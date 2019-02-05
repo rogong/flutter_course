9c3c71d9589687797689c9b9d34b3a24f071a8a4
rogong <itingasng@gmail.com>
2019-02-05 16:24:06 +0100

    first commit

---
+++ lib/product_manager.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import './products.dart';
+import './product_control.dart';
+
+class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager({this.startingProduct}) {}
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    if (widget.startingProduct != null) {
+      _products.add(widget.startingProduct);
+    }
+    super.initState();
+  }
+
+  @override
+  void didUodateWidget(ProductManager oldWidget) {
+    print('[ProductManager State] didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void _addProducts(String product) {
+    setState(() {
+      _products.add(product);
+    });
+    print(_products);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: ProductControl(_addProducts),
+        ),
+        Expanded(child: Products(_products))
+      ],
+    );
+  }
+}
---
+++ lib/products.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Products extends StatelessWidget {
+  final List<String> products;
+
+  Products([this.products = const []]) {}
+
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductList() {
+    Widget productCards;
+    if (products.length > 0) {
+      productCards = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCards = Container();
+    }
+    return productCards;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildProductList();
+  }
+}
